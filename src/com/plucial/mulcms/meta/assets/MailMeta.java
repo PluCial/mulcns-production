9b474cb0fa88bd2f42a24d52ebb7c5af0daeb655
takaharakou <takaharakou@takaharaisaonomacbook-puro.local>
2019-01-22 11:37:02 +0900

    バージョンアップJava

--- src/com/plucial/mulcms/meta/assets/MailMeta.java
+++ src/com/plucial/mulcms/meta/assets/MailMeta.java
@@ -1,6 +1,6 @@
 package com.plucial.mulcms.meta.assets;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2019-01-11 14:32:09")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2019-01-19 14:57:27")
 /** */
 public final class MailMeta extends org.slim3.datastore.ModelMeta<com.plucial.mulcms.model.assets.Mail> {
 
--- src/com/plucial/mulcms/meta/widgets/form/FormControlMeta.java
+++ src/com/plucial/mulcms/meta/widgets/form/FormControlMeta.java
@@ -1,6 +1,6 @@
 package com.plucial.mulcms.meta.widgets.form;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2019-01-11 14:32:09")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2019-01-19 14:57:27")
 /** */
 public final class FormControlMeta extends org.slim3.datastore.ModelMeta<com.plucial.mulcms.model.widgets.form.FormControl> {
 
